b853942bacaaeaf4ae8ec19f54e628f2ce3038d2
cany748 <cany748@gmail.com>
2025-02-27 18:26:42 +0700

    feat: update dependencies

--- tsup.config.ts
+++ tsup.config.ts
@@ -4,4 +4,5 @@ export default defineConfig({
   entry: ["src/index.ts"],
   shims: true,
   external: ["@typescript-eslint/eslint-plugin", "@typescript-eslint/parser", "vue-eslint-parser"],
+  format: ["esm"],
 });
